7b7ea994ed17227032039292994f765b3bb3d77c
skynereidof <63476569+skynereidof@users.noreply.github.com>
2023-03-11 15:10:27 +0100

    Update setting.py

--- lxdesetting/setting.py
+++ lxdesetting/setting.py
@@ -14,7 +14,10 @@ applications = [
     {'name': 'Lxrand', 'command': 'lxrand'},
     {'name': 'Grub Customizer', 'command': 'grub-customizer'},
     {'name': 'HP Toolbox', 'command': 'hp-toolbox'},
-    {'name': 'Gparted', 'command': 'gparted'}
+    {'name': 'Gparted', 'command': 'gparted'},
+    {'name': 'Nitrogen', 'command': 'nitrogen'},
+    {'name': 'Conky Manager', 'command': 'conky-manager'},
+    {'name': 'LXDM Setting', 'command': 'sudo lxdm-config'}
 ]
 
 # funkcja uruchamiająca wybrany program
@@ -27,21 +30,33 @@ def close_program():
 
 # stworzenie okna
 window = tk.Tk()
-window.title('Aktywatory')
+window.title('LXDM Setting')
+window.configure(bg='white')
 
-# stworzenie przycisków dla każdego aktywatora
+# stworzenie etykiet z nazwami programów i przycisków dla każdego aktywatora
 num_rows = (len(applications) + 1) // 2
 for i in range(num_rows):
     for j in range(2):
         index = i * 2 + j
         if index < len(applications):
-            app_button = tk.Button(window, text=applications[index]['name'], 
+            app_label = tk.Label(window, text=applications[index]['name'])
+            app_label.grid(row=i, column=j*2, padx=5, pady=5, sticky='e')
+            
+            app_button = tk.Button(window, text='Uruchom', 
                                    command=lambda index=index: run_application(index))
-            app_button.grid(row=i, column=j, padx=5, pady=5)
+            app_button.grid(row=i, column=j*2+1, padx=5, pady=5)
 
 # stworzenie przycisku zamykającego program
 close_button = tk.Button(window, text='Zamknij', command=close_program)
-close_button.grid(row=num_rows, column=0, columnspan=2, pady=10)
+close_button.grid(row=num_rows+1, column=0, columnspan=2, pady=10)
+
+# przycisk do włączania i wyłączania programu Xcompmgr
+xcompmgr_button = tk.Button(window, text='Włącz Xcompmgr', command=lambda: os.system('xcompmgr -c -f -F -D 5 &'))
+xcompmgr_button.grid(row=num_rows, column=0, padx=5, pady=5)
+
+kill_xcompmgr_button = tk.Button(window, text='Wyłącz Xcompmgr', command=lambda: os.system('killall xcompmgr'))
+kill_xcompmgr_button.grid(row=num_rows, column=1, padx=5, pady=5)
 
 # uruchomienie głównej pętli programu
 window.mainloop()
+
